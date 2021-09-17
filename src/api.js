328f60a1161f64c71a8ca5517a6f10ed0ce61889
Pierina Montalva <pierinamfatur@gmail.com>
2021-09-17 15:42:15 -0500

    Se agregó la función para ver si el path existe

--- src/api.js
+++ src/api.js
@@ -1,20 +1,9 @@
-import { access, F_OK } from 'fs';
+import { existsSync } from 'fs';
 // import * as path from 'path';
-import chalk from 'chalk'; // para añadir color al texto
+// import chalk from 'chalk'; // para añadir color al texto
 
-// const manualPath = 'C:\\Users
-// \\user\\Desktop\\LABORATORIA\\LIM015-md-links\\validator\\validator.md';
+const directPath = '../validator/validator.md'; // prueba
 
-const manualPath = '../validator/validator.ms';
-
-const validatePath = () => {
-  access(manualPath, F_OK, (err) => {
-    if (err) {
-      console.error(chalk.red('nonexistent file ❌'));
-    }
-  });
-};
-
-validatePath();
-
-// node --trace-uncaught ...
+// ---------- Para saber si el path existe ---------------- //
+const isAnExistingPath = (track) => existsSync(track);
+console.log(isAnExistingPath(directPath)); // prueba
