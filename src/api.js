e37a8ea7466a105605a20fa2560b346a96bb9a5a
Pierina Montalva <pierinamfatur@gmail.com>
2021-09-17 16:38:58 -0500

    se añadio función para saber si el path es un directorio

--- src/api.js
+++ src/api.js
@@ -1,9 +1,26 @@
-import { existsSync } from 'fs';
-// import * as path from 'path';
+import { existsSync, stat } from 'fs';
+import * as path from 'path';
 // import chalk from 'chalk'; // para añadir color al texto
 
 const directPath = '../validator/validator.md'; // prueba
 
-// ---------- Para saber si el path existe ---------------- //
-const isAnExistingPath = (track) => existsSync(track);
+// ---------------------- Para saber si el path existe ----------------------------- //
+const isAnExistingPath = (track) => existsSync(track); // true o false
 console.log(isAnExistingPath(directPath)); // prueba
+
+// -------------------- Para saber si el path es absoluto -------------------------- //
+const isAbsolutePath = (track) => path.isAbsolute(track); // true o false
+console.log(isAbsolutePath(directPath)); // prueba
+
+// --------------------- Para saber si el path es un directorio -------------------- //
+const isAdirectory = (track) => {
+  stat(track, (error, stats) => {
+    if (error) {
+      console.error(error);
+    }
+    // chequea si el path es o no un directorio
+    console.log(stats.isDirectory()); // true o false
+  });
+};
+
+isAdirectory(directPath); // prueba
