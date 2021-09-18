ac53eb0301a21207acb5e18928d6bd0b35ef468e
Pierina Montalva <pierinamfatur@gmail.com>
2021-09-18 18:01:29 -0500

    se agregó comentario en el api.spec.js

--- test/api.spec.js
+++ test/api.spec.js
@@ -4,10 +4,9 @@ import * as api from '../src/api.js'
 
 const track = '../validator/validator.md';
 
-console.log(api.isAnExistingPath());
 describe('isAnExistingPath', () => {
     it('Debe validar si el path existe', () => {
-      expect(isAnExistingPath(track).toBe(true))
+      expect(api.isAnExistingPath(track).toBe(true))
     });
   });
   
