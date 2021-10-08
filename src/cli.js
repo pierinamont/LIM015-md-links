c52f3cfb869b810d131b8d83d463f48c81b239f9
Pierina Montalva <pierinamfatur@gmail.com>
2021-10-08 15:46:15 -0500

    se terminó README.md

--- src/cli.js
+++ src/cli.js
@@ -54,11 +54,11 @@ if (args.length === 1) {
   } else {
     md.mdLinks(args[0], { validate: false })
       .then((array) => array.map((element) => {
-        const file = chalk.bold.grey.bgWhite(element.file);
+        const file = chalk.bold.grey(element.file);
         const href = chalk.underline.cyan(element.href);
         const text = chalk.italic.magenta(element.text);
 
-        console.log(`${file} ${href} ${text}`);
+        console.log(`${file}\n${href}\n${text}\n`);
         return `${file} ${href} ${text}`;
       }))
       .catch((err) => console.log(err));
@@ -70,13 +70,13 @@ if (args.length === 2) {
   if (args[1] === '--validate') {
     md.mdLinks(args[0], { validate: true })
       .then((array) => array.map((element) => {
-        const file = chalk.bold.grey.bgWhite(element.file);
+        const file = chalk.bold.grey(element.file);
         const href = chalk.underline.cyan(element.href);
         const text = chalk.italic.magenta(element.text);
         const status = chalk.yellow(element.status);
         const statusText = chalk.blue(element.statusText);
 
-        console.log(`${file} ${href} ${text} ${status} ${statusText}`);
+        console.log(`${file}\n${href}\n${text}\n${status}\n${statusText}\n`);
         return `${file} ${href} ${text} ${status} ${statusText}`;
       }))
       .catch((err) => console.log(err));
